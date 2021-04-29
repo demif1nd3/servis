14b8366cb3c1c80b596017772a301b482150f3a7
demif1nd3 <83338135+demif1nd3@users.noreply.github.com>
2021-04-29 04:15:37 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -6,8 +6,10 @@ include 'database.php';
 <section class="fon">
 <div class="fon1"><h1>SMOKIN SICK STYLE</h1></div>
 <h1 align="center">ЛУЧШИЙ ВЫБОР СРЕДИ АВТОСЕРВИСОВ РОССИИ</h1><br>
+
 	    <div class="container">
             <div class=".align-content-center"> 
+
   </div>
 </div>
   </section>
@@ -16,17 +18,17 @@ include 'database.php';
 <div class="row">
            <form  method="POST"  role="form" class="form-inline">
            <img align="left" src="img/pp4.jpg" width="300%" height="450" class="img1">
-           <h1 align="center">Наши сервисы на яндекс карте</h1><br>
-           <p><select name="select" size="1" multiple>
-              <option selected value="s1">Автосервис ССС филиал 1</option>
-              <option value="s2">Автосервис ССС филиал 2</option>
-          </select>
-          <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A7429d65e74c8f8511a9eb2d3e7cfcb2bfa2755a14bd12c870f9af4b8a150460a&amp;width=1104&amp;height=574&amp;lang=ru_RU&amp;scroll=true"></script>
-         <br>
+           <h1 align="center">Наши сервисы на карте</h1><br><br>&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
+
+           <select onchange="document.getElementById('preview').src = this.value">
+    <option value="img/pp6.jpg">Автосервис ССС филиал 2</option>
+    <option value="img/pp7.jpg">Автосервис ССС филиал 1</option>
+</select>
+
+<img id="preview">
 
          </div>  </div>
           </form>
-          </div>
-          <div id="myModal" class="modal fade">
+          </div></div>
           
 <?php include 'temp/footer.php'; ?>
